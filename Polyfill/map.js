8037ea8f3a5206213dbc4cbd4a95d474c130459c
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-02-28 19:26:25 +0530

    Update map.js

--- Polyfill/map.js
+++ Polyfill/map.js
@@ -1,16 +1,16 @@
-let arr = [1, 2, 3, 4, 5, 6]
+let arr = [1, 2, 3, 4, 5, 6];
 
-Array.prototype.map = null
+Array.prototype.map = null;
 
-if(!Array.prototype.map) {
-    Array.prototype.map = function(cb) {
-        let resArr = []
-        for(const index in this) {
-            resArr.push(cb(this[index], index, this))
-        }
-        return resArr
+if (!Array.prototype.map) {
+  Array.prototype.map = function (cb) {
+    let resArr = [];
+    for (const index in this) {
+      resArr.push(cb(this[index], index, this));
     }
+    return resArr;
+  };
 }
 
-let newArr = arr.map((element, index, array) => array)
-console.log(newArr)
+let newArr = arr.map((element, index, array) => array);
+console.log(newArr);
